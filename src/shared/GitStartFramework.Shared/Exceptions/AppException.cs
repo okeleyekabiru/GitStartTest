4c7a48b087ab838547458e66192fde12a31d71c0
okeleyekabiru <kabiotobiano@gmail.com>
2024-09-21 18:59:03 +0100

    fix docker compose issue

---
+++ src/shared/GitStartFramework.Shared/Exceptions/AppException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitStartFramework.Shared.Exceptions
+{
+    public class AppException : Exception
+    {
+        public AppException()
+        {
+                
+        }
+    }
+}
